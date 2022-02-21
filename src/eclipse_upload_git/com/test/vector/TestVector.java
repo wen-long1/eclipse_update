a6f339f261611c1b6668c1d73ab3b4f66431007d
wen-long1 <a1479998076a@163.com>
2022-02-21 22:04:05 +0800

    测试switch关键字

---
+++ src/eclipse_upload_git/com/test/vector/TestVector.java
@@ -0,0 +1,29 @@
+package eclipse_upload_git.com.test.vector;
+
+import java.util.Vector;
+
+public class TestVector {
+
+	public static void main(String[] args){
+		
+		Vector vt = null;
+		
+		String str = "3";
+		switch(str){
+			case "1": 
+				System.out.println("执行1");
+				break;
+			case "2":
+				System.out.println("执行2");
+				break;
+			case "3":
+				System.out.println("执行3");
+				break;
+			case "4":
+				System.out.println("执行4");
+				break;
+		}
+		
+	}
+	
+}
